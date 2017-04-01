bd58d1e3e003b975be7a4086ba203e83c2fa900d
anthonyjuan <anthonyjuan95@gmail.com>
2017-04-01 18:50:19 +0700

    first commit

---
+++ javascript-refactor/assets/javascript/app.js
@@ -0,0 +1,49 @@
+
+
+class diceView {
+
+  static appendDice() {
+    $('#roller').append('<div class="dice">0</div>')
+  }
+
+  static setValue() {
+    $('.dice').each(function() {
+      let value = diceObject.randNumber();
+      $(this).text(value);
+    })
+  }
+
+}
+
+
+class diceModel {
+  constructor() {
+
+  }
+}
+
+class diceController {
+
+  addDice() {
+    diceView.appendDice();
+  }
+
+  rollDice() {
+    diceView.setValue();
+  }
+  randNumber() {
+    return Math.floor((Math.random()*6)+1)
+  }
+
+
+}
+
+
+let diceObject = new diceController();
+
+
+
+$(document).ready(function() {
+  $('.add').on('click', diceObject.addDice);
+  $('.roll').on('click', diceObject.rollDice)
+})
